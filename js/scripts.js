9205e0710c94e604d99d3f7ccc3508a2ce86a6fd
unknown <gfontenele240@gmail.com>
2022-10-26 06:34:46 -0300

    Modal de login adicionado

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,3 @@ import { getFirestore, collection, addDoc } from "https://www.gstatic.com/fireba
 
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
-/*
-  try {
-    const docRef = await addDoc(collection(db, "users"), {
-      first: "Gustavo",
-      last: "Lovecraft",
-      born: 1815
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-*/
