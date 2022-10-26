323b9ab781d16f521860053d02fd6de9c246a57e
Gustavo Brian <gustavo-dm2010@hotmail.com>
2022-10-26 16:26:55 -0300

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js"
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCAZoxLc4Q-bOzuma-qiIxanO8haVznHCI",
+    authDomain: "concord-v1.firebaseapp.com",
+    projectId: "concord-v1",
+    storageBucket: "concord-v1.appspot.com",
+    messagingSenderId: "373783464860",
+    appId: "1:373783464860:web:8dce1a8eb7fb8d66805659"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+/*
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: "Gustavo",
+      last: "Lovecraft",
+      born: 1815
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+*/
